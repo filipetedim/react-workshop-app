0488b0a5cc5e80d8d11957d54b9d7f4a21d1c67b
Filipe Tedim <filipetedim@gmail.com>
2020-01-24 03:35:14 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Theme
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello jangexs</div>
+    );
+  }
+}
+export default App;
